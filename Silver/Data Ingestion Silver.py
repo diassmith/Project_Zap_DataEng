357dfd670250cd0b77ba9a09004f586a5cf8eb6a
dias_wise@hotmail.com <dias_wise@hotmail.com>
2022-04-03 20:49:59 +0000

    Finalizing Conection
    Finalizing the connection between Databricks and Azure Data Lake Storage with Azure Key Vault

--- Silver/Data Ingestion Silver.py
+++ Silver/Data Ingestion Silver.py
@@ -1,8 +1,14 @@
 # Databricks notebook source
 # DBTITLE 1,Doing the Conections with ADLS
 spark.conf.set(
-    "fs.azure.account.key.dlsturma01imersao.dfs.core.windows.net",
+    "fs.azure.account.key.dlszap.dfs.core.windows.net",
     dbutils.secrets.get(scope="keyvaultzap",key="dlzapKey"))
+
+# COMMAND ----------
+
+# DBTITLE 1,Checking all scopes created
+# MAGIC %python
+# MAGIC dbutils.secrets.listScopes()
 
 # COMMAND ----------
 
@@ -13,8 +19,25 @@ configs = {"fs.azure.account.auth.type": "OAuth",
        "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="keyvaultzap",key="secret"),
        "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/"+dbutils.secrets.get(scope="keyvaultzap", key="directoryTenantId")+"/oauth2/token"}
 
+# COMMAND ----------
+
+# DBTITLE 1, Create mount point on layers
+zones = ["bronze", "silver", "gold"]
+for zone in zones:
+    try:
+        dbutils.fs.mount(
+            source = "abfss://"+zone+"@dlszap.dfs.core.windows.net/",
+            mount_point = "/mnt/"+zone+"/",
+            extra_configs = configs)
+    except:
+            print("")
 
 # COMMAND ----------
 
-# MAGIC %python
-# MAGIC dbutils.secrets.listScopes()
+# DBTITLE 1,Check mount CLI Linux
+# MAGIC %fs ls /mnt/bronze/
+
+# COMMAND ----------
+
+# DBTITLE 1,Check mount with Python
+dbutils.fs.ls ("/mnt/bronze")
